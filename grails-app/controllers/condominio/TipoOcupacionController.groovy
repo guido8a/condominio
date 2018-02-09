f0708ec29c5aa2f9b568c558eb5b42fa8eadff93
guido8a <guido8a@gmail.com>
2018-02-09 11:26:27 -0500

    shield

--- grails-app/controllers/condominio/TipoOcupacionController.groovy
+++ grails-app/controllers/condominio/TipoOcupacionController.groovy
@@ -1,12 +1,13 @@
 package condominio
 
 import org.springframework.dao.DataIntegrityViolationException
+import seguridad.Shield
 
 
 /**
  * Controlador que muestra las pantallas de manejo de TipoOcupacion
  */
-class TipoOcupacionController extends seguridad.Shield {
+class TipoOcupacionController extends Shield {
 
     static allowedMethods = [save_ajax: "POST", delete_ajax: "POST"]
 
