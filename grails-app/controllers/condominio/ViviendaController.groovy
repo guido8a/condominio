5deb6d141264894fd9b30c4397e2e858a3697a07
guido8a <guido8a@gmail.com>
2020-09-06 10:56:11 -0500

    descriptivo

--- grails-app/controllers/condominio/ViviendaController.groovy
+++ grails-app/controllers/condominio/ViviendaController.groovy
@@ -97,6 +97,10 @@ class ViviendaController extends Shield {
         def band = new Date().before(fecha)
 
         println "fecha: $fecha"
+        println "data: $data"
+        println "params: $params"
+        println "band: $band"
+        println "oblg: $oblg"
         [personas: data, params: params, oblg: oblg, band: band]
     }
 
